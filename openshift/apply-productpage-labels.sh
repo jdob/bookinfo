a713c59118ad484daec0643761e3db13a6e07ebb
jdob <jdobies@redhat.com>
2021-02-12 16:08:00 -0500

    added productpage labels

--- openshift/apply-productpage-labels.sh
+++
@@ -1,4 +0,0 @@
-oc project user1-bookinfo && \
-oc label deployment/productpage app.openshift.io/runtime=python --overwrite && \
-oc label deployment/productpage app.kubernetes.io/part-of=bookinfo --overwrite && \
-oc annotate deployment/productpage app.openshift.io/connects-to=details
